bbd4c2745f8d1e23cd8ca6f229e9c17ff659ca6d
heriveltogabriel <heriveltogabriel@gmail.com>
2026-05-27 11:14:24 -0300

    Change Total Fim do Mês (CDB+Aporte) and Valor Total da Carteira in history table to be dynamically computed at runtime

--- static/app.js
+++ static/app.js
@@ -1447,7 +1447,7 @@ function renderHistoryTables() {
                     
                     let val = 0;
                     if (mr.type === 'total') {
-                        val = bankData.total !== undefined ? bankData.total : (bankData.cdb || 0) + (bankData.aporte || 0);
+                        val = (parseFloat(bankData.cdb) || 0) + (parseFloat(bankData.aporte) || 0);
                     } else {
                         val = bankData[mr.type] || 0;
                     }
@@ -1494,16 +1494,12 @@ function renderHistoryTables() {
                 let sum = 0;
                 
                 if (mData.investments) {
-                    if (mr.type === 'total' && mData.total_carteira !== undefined) {
-                        sum = mData.total_carteira;
-                    } else {
-                        for (const b in mData.investments) {
-                            const bankData = mData.investments[b] || {};
-                            if (mr.type === 'total') {
-                                sum += bankData.total !== undefined ? bankData.total : (bankData.cdb || 0) + (bankData.aporte || 0);
-                            } else {
-                                sum += bankData[mr.type] || 0;
-                            }
+                    for (const b in mData.investments) {
+                        const bankData = mData.investments[b] || {};
+                        if (mr.type === 'total') {
+                            sum += (parseFloat(bankData.cdb) || 0) + (parseFloat(bankData.aporte) || 0);
+                        } else {
+                            sum += bankData[mr.type] || 0;
                         }
                     }
                 }
